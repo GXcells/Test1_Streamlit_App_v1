1b211476eb65eab406d6eb7bda095d443a9b8a71
GXcells <gxcells61231@gmail.com>
2024-02-23 09:22:01 +0100

    First ligation calculator

--- pages/2_🚀_page 2.py
+++ pages/2_🚀_page 2.py
@@ -1,18 +1,17 @@
 import streamlit as st
-from audiorecorder import audiorecorder
+import time
 st.set_page_config(page_title="Page 2", page_icon="🚀")
 
+st.title("Ligation Calculator")
+with st.form(key='ligation_calculator'):
+    vect_size = st.number_input('Vector size in bp',value=None, format="%.1f",placeholder="Type vector size")
+    vect_amt = st.number_input('Vector amount in ng',value=None, format="%.1f", placeholder="Type vector amount")
+    insert_size = st.number_input('Insert size in bp',value=None, format="%.1f",placeholder="Type insert size")
+    vect_ratio = st.selectbox("Vector:insert ratio", options=["1:1","1:2","1:3","1:4","1:5","1:7"],key="kvect_ratio")
+    
+    #b = st.number_input('b')
+    submit = st.form_submit_button('Calculate')
+    if submit:
+        submit_toast=st.toast("Thanks")
 
-
-st.title("Audio Recorder")
-audio = audiorecorder("Click to record", "Click to stop recording")
-
-if len(audio) > 0:
-    # To play audio in frontend:
-    st.audio(audio.export().read())  
-
-    # To save audio to a file, use pydub export method:
-    audio.export("audio.wav", format="wav")
-
-    # To get audio properties, use pydub AudioSegment properties:
-    st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+  
